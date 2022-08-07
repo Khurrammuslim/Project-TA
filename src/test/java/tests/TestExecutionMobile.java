1d09c7bc4982190a442afdf42ee0b567b647c4c1
Khurram Muslim <Khurram.muslim@gmail.com>
2022-08-08 04:35:24 +0500

    Addressing Centrepoint VPN issue

--- src/test/java/tests/TestExecutionMobile.java
+++ src/test/java/tests/TestExecutionMobile.java
@@ -11,6 +11,17 @@ import utils.TestListener;
 @Listeners(TestListener.class)
 public class TestExecutionMobile extends MobileCorePage {
 
+/* 
+ * I am unable to run Centrepoint app on either emulator or android mobile phone because
+ * of the limitation of Centrepoint app that requires VPN access to eiher UAE or UK VPN.
+ * I have tried couple of free VPN to figure this out and tries to run Centrepoint app
+ * but not able to do so. I am informing you as I have completed all tasks from the 
+ * assignment but if you guys think you can send me different app that will not have
+ * VPN access requirement as I am based in Pakistan.
+ * 
+ */
+	
+	
 	@Test(description = "Mobile-test")
 	public void sampletest() {
 		MobileProductPage.test1();
